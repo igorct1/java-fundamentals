b18a782dee69b87b3dc54824d524d37a752c7af9
igorct1 <igorct77@gmail.com>
2024-06-03 18:07:33 -0300

    tipos primitivos - exercicio

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,16 +2,8 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        // int, double, float, char, byte, short, long, boolean
-        int firstNumber = 10;
-        long higherNumber = 10000;
-        char character = 'M';
-        double salary = 2000;
-        float salaryFloat = 2500;
-        byte ageByte = 29;
-        short ageShort = 10;
-        boolean canDrive = true;
-        String name = "Igor";
-        System.out.println(name);
+        String firstName = "Igor";
+        String fullName = "Igor Tozetti";
+        System.out.println(fullName);
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+/*
+    Prática
+    eu <nome>, morando no endereco <endereco>,
+    confirmo que recebi o <salario>, na data <data>
+ */
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Igor";
+        String address = "Rua Alexandre Correia 173";
+        int salary = 2100;
+        String data = "24/05/2024";
+        String relatory = "Eu " +name+ " morando no endereco: "+address+" confirmo que recebi o salário "+salary+" na data: "+data;
+        System.out.println(relatory);
+    }
+}
