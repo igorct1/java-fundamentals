96d3455086a0915dca33a3f4038cb5b3e4eea2d7
igorct1 <igorct77@gmail.com>
2024-06-03 13:36:35 -0300

    tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int firstNumber = 10;
+        long higherNumber = 10000;
+        char character = 'M';
+        double salary = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 29;
+        short ageShort = 10;
+        boolean canDrive = true;
+        String name = "Igor";
+        System.out.println(name);
+    }
+}
