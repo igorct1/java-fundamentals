d49b392b1773f9fa10d6124b4f51e554929dd076
igorct1 <igorct77@gmail.com>
2024-06-11 11:06:29 -0300

    oop - sobrescrita do toString

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescrita.model.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Solo Leveling");
+
+        System.out.println(anime);
+    }
+}
