144f7ec7ed288af93693d70445ae179f24978758
igorct1 <igorct77@gmail.com>
2024-06-11 10:02:34 -0300

    oop - heranca (super)

--- src/academy/devdojo/maratonajava/javacore/Hheranca/model/Employer.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/model/Employer.java
@@ -3,6 +3,12 @@ package academy.devdojo.maratonajava.javacore.Hheranca.model;
 public class Employer extends Person {
     private double salary;
 
+    @Override
+    public void print(){
+        super.print();
+        System.out.println(this.salary);
+    }
+
     public double getSalary() {
         return salary;
     }
