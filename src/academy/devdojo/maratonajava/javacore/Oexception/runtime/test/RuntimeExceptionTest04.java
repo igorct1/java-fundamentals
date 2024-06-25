be0c8cfb1f408c7297d1d7d7cdf8ad5c2a05ac25
igorct1 <igorct77@gmail.com>
2024-06-25 11:09:33 -0300

    exceptions - capturando varias exceptions

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndex");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOut");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro de IllegalArgument");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro de ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro de RuntimeException");
+        }
+    }
+
+}
