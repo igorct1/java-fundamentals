16e0ec41287dbbbc51d35db04cb8194ed871aa5c
igorct1 <igorct77@gmail.com>
2024-06-25 10:37:01 -0300

    exceptions - lancando excecoes checked

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -3,12 +3,12 @@ package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
 import java.io.File;
 import java.io.IOException;
 
-public class ExceptionTest01 {
-    public static void main(String[] args) {
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException {
         createFile();
     }
 
-    private static void createFile() {
+    private static void createFile() throws IOException {
         File file = new File("file/test.txt");
 
         try {
@@ -16,6 +16,7 @@ public class ExceptionTest01 {
             System.out.println("Arquivo criado: " + isCreated);
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
     }
 }
