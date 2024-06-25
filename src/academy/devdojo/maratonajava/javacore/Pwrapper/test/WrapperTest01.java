615fc724fc29f676e5c299e770c697dd6b295e08
igorct1 <igorct77@gmail.com>
2024-06-25 19:45:57 -0300

    classes utilitarias - wrappers

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        int intP = 1;
+        short shortP = 1;
+        byte byteP = 1;
+        long longP = 10;
+        double doubleP = 10;
+        float floatP = 10;
+        char charP = 'A';
+        boolean booleanP = true;
+
+        Integer intW = 1;
+        Short shortW = 1;
+        Byte byteW = 1;
+        Long longW = 10L;
+        Double doubleW = 10D;
+        Float floatW = 10F;
+        Character charW = 'A';
+        Boolean boolW = true;
+    }
+}
