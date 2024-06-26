fc5b49c49f79482af4125892478587d5df32a052
igorct1 <igorct77@gmail.com>
2024-06-26 20:10:31 -0300

    classes utilitarias - string (desempenho/stringbuilder)

---
+++ src/academy/devdojo/maratonajava/javacore/Pstring/test/StringPerformanceTest.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Pstring.test;
+
+public class StringPerformanceTest {
+
+    public static void main(String[] args) {
+        long in = System.currentTimeMillis();
+        concatString(30_000);
+        long fi = System.currentTimeMillis();
+        System.out.println("tempo string: " + (fi - in) + "ms");
+
+        long in2 = System.currentTimeMillis();
+        concatStringBuilder(100_000);
+        long fi2 = System.currentTimeMillis();
+        System.out.println("tempo stringbuilder: " + (fi2 - in2) + "ms");
+    }
+
+    public static void concatString(int size) {
+        java.lang.String text = "";
+        for (int i = 0; i < size; i++) {
+            text += i;
+        }
+    }
+
+    public static void concatStringBuilder(int size) {
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            text.append(i);
+        }
+    }
+}
