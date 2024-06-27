7d63d57d5bff0d53827f15c32482c2885c9a4c39
igorct1 <igorct77@gmail.com>
2024-06-27 13:28:33 -0300

    classes utilitarias - datas (date/calendar)

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/CalendarTest.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        Date date = c.getTime();
+        System.out.println(date);
+
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(); // long millis
+        System.out.println(date);
+    }
+}
