2d438b77440f61f02ea8cbd302b0197e7b1951ef
igorct1 <igorct77@gmail.com>
2024-06-27 14:00:03 -0300

    classes utilitarias - locale

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/LocaleTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeItaly = new Locale("it", "it");
+        DateFormat brDate = DateFormat.getDateInstance(DateFormat.FULL, localeBR);
+        DateFormat italyDate = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        System.out.println(brDate.format(c.getTime()));
+        System.out.println(italyDate.format(c.getTime()));
+    }
+}
