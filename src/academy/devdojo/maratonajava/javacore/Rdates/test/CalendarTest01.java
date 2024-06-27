24d6e3292daf9689915d66fb7effa2e2e733e771
igorct1 <igorct77@gmail.com>
2024-06-27 13:39:19 -0300

    classes utilitarias - DateFormat

--- src/academy/devdojo/maratonajava/javacore/Rdates/test/CalendarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/CalendarTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Rdates.test;
 import java.util.Calendar;
 import java.util.Date;
 
-public class CalendarTest {
+public class CalendarTest01 {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
         Date date = c.getTime();
